f1c9aa4e171a4014f977ffc76eba5e1fb4593869
doloresdecabeza <33382865+doloresdecabeza@users.noreply.github.com>
2019-01-23 10:37:01 -0400

    Quitando "imports" innecesarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,15 @@
 import BBDD.Conection;
-import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class Main extends Application {
+
     public static void main(String args[]) throws IOException, ExecutionException, InterruptedException {
 
         try{
