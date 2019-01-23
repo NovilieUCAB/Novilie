905ce37d87ff2737df37f0fc963398a7712c090a
Tania Riobueno <tanialita2308@gmail.com>
2019-01-23 00:37:02 -0400

    Clase "Conection" Contiene la implementación para la conexión con la database en Firebase. Y su debida modificación en el main para su preparada ejecución.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import BBDD.Conection;
+import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,22 +14,31 @@ import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class Main extends Application {
-    public static void main(String args[]) throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("novilie-firebase-adminsdk-od77w-74a1f74d4a.json");
+    public static void main(String args[]) throws IOException, ExecutionException, InterruptedException {
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://novilie.firebaseio.com")
-                .build();
+        try{
+            Conection db = new Conection();
 
-        FirebaseApp.initializeApp(options);
+            // Create a Map to store the data we want to set
+            /*Map<String, Object> docData = new HashMap<>();
+            docData.put("name", "Los Angeles");
+            docData.put("state", "CA");
+            docData.put("country", "USA");*/
 
+            // Add a new document (asynchronously) in collection "cities" with id "LA"
+            //ApiFuture<WriteResult> future = db.getDatabase().collection("cities").document("LA").set(docData);
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
         launch(args);
     }
-
 
     @Override
     public void start(Stage primaryStage) throws Exception {
