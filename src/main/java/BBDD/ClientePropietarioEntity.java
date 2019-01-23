e508890d2349beb31c5701472758d0d45292099c
MauricioMorales <mauricio1999morales@gmail.com>
2019-01-23 18:01:59 -0400

    Clase Cliente Propietario: actualizada

--- src/main/java/BBDD/ClientePropietarioEntity.java
+++ src/main/java/BBDD/ClientePropietarioEntity.java
@@ -7,10 +7,10 @@ public class ClientePropietarioEntity extends ClienteEntity {
 
 
     public ClientePropietarioEntity(int cedula, String nombre, String apellido, int edad, int diaNacimiento, int mesNacimiento,
-                                    int añoNacimiento, String email, int telefono, String trabajo, String parroquiaDireccion,
+                                    int anhoNacimiento, String email, int telefono, String trabajo, String parroquiaDireccion,
                                     String municipioDireccion, String estadoDireccion, String ciudadDireccion, String direccion,
                                     float precioInicial) {
-        super(cedula, nombre, apellido, edad, diaNacimiento, mesNacimiento, añoNacimiento, email, telefono, trabajo,
+        super(cedula, nombre, apellido, edad, diaNacimiento, mesNacimiento, anhoNacimiento, email, telefono, trabajo,
                 parroquiaDireccion, municipioDireccion, estadoDireccion, ciudadDireccion, direccion);
     }
 
