1b4d510f9966dfcc2ca469bbef6f5744bf81389a
MauricioMorales <mauricio1999morales@gmail.com>
2019-01-23 17:22:23 -0400

    Clase Cliente Potencial: nueva Clase

---
+++ src/main/java/BBDD/ClientePotencialEntity.java
@@ -0,0 +1,23 @@
+package BBDD;
+
+public class ClientePotencialEntity extends ClienteEntity{
+    boolean operacion;
+
+    public ClientePotencialEntity(int cedula, String nombre, String apellido, int edad, int diaNacimiento, int mesNacimiento,
+                                  int añoNacimiento, String email, int telefono, String trabajo, String parroquiaDireccion,
+                                  String municipioDireccion, String estadoDireccion, String ciudadDireccion, String direccion,
+                                  boolean operacion) {
+
+        super(cedula, nombre, apellido, edad, diaNacimiento, mesNacimiento, añoNacimiento, email, telefono, trabajo,
+                parroquiaDireccion, municipioDireccion, estadoDireccion, ciudadDireccion, direccion);
+        this.operacion=operacion;
+    }
+
+    public boolean getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(boolean operacion) {
+        this.operacion = operacion;
+    }
+}
