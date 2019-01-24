0444bf67786fe0779e1c1e7c875843adb6332b89
Tania Riobueno <tanialita2308@gmail.com>
2019-01-24 17:46:24 -0400

    Algunos Cambios

--- src/main/java/BBDD/ModelosBD/ClientePropietarioEntity.java
+++ src/main/java/BBDD/ModelosBD/ClientePropietarioEntity.java
@@ -1,10 +1,26 @@
 package BBDD.ModelosBD;
 
+import BBDD.Fecha;
+import BBDD.Telefono;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class ClientePropietarioEntity extends ClienteEntity {
-    private ArrayList<InmuebleEntity> inmuebles = new ArrayList<>();
+    private ArrayList<Integer> inmuebles = new ArrayList<>();
 
+    public ClientePropietarioEntity(String cedula, String nombre, String apellido, Fecha fechaNacimiento, String email,
+                                  String trabajo, String estadoDireccion, String ciudadDireccion, String direccion, ArrayList<Integer> inmuebles){
+        super(cedula,nombre,apellido,fechaNacimiento,email,trabajo,estadoDireccion,ciudadDireccion,direccion);
+        this.inmuebles = inmuebles;
+    }
+
+    public ClientePropietarioEntity(String cedula, String nombre, String apellido, Fecha fechaNacimiento, String email,
+                                  List<Telefono> telefono, String trabajo, String parroquiaDireccion, String municipioDireccion,
+                                  String estadoDireccion, String ciudadDireccion, String direccion, ArrayList<Integer> inmuebles){
+        super(cedula,nombre,apellido,fechaNacimiento,email,telefono,trabajo,parroquiaDireccion,municipioDireccion,estadoDireccion,ciudadDireccion,direccion);
+        this.inmuebles = inmuebles;
+    }
 
     /*public ClientePropietarioEntity(int cedula, String nombre, String apellido, int edad, int diaNacimiento, int mesNacimiento,
                                     int anhoNacimiento, String email, int telefono, String trabajo, String parroquiaDireccion,
@@ -14,11 +30,18 @@ public class ClientePropietarioEntity extends ClienteEntity {
                 parroquiaDireccion, municipioDireccion, estadoDireccion, ciudadDireccion, direccion);
     }*/
 
-    public void addInmueble(InmuebleEntity inmueble) {
+    public void addInmueble(int inmueble) {
         this.inmuebles.add(inmueble);
     }
 
-    public void removeInmueble(InmuebleEntity inmueble) {
+    public void removeInmueble(int inmueble) {
         this.inmuebles.remove(inmueble);
+    }
+
+    @Override
+    public String toString() {
+        return "ClientePropietarioEntity{" +
+                "inmuebles=" + inmuebles.toString() +
+                '}';
     }
 }
