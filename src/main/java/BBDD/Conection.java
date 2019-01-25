bc3ed4bef280eb69f8f6e3ae1e234a48f7829749
Tania Riobueno <tanialita2308@gmail.com>
2019-01-25 00:32:02 -0400

    Nueva clase EmailMarketingController Contiene todas los metonos que FRONT utilizara para hacer CRUD

--- src/main/java/BBDD/Conection.java
+++ src/main/java/BBDD/Conection.java
@@ -1,15 +1,12 @@
 package BBDD;
 
-import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Conection {
