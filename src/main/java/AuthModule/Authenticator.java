d1c9ea94e7471204dbcf1a6acfc15e6ab9b72178
hazudesu <46248038+hazudesu@users.noreply.github.com>
2019-01-25 20:21:10 -0400

    -modificaciones de clase authenticator

--- src/main/java/AuthModule/Authenticator.java
+++ src/main/java/AuthModule/Authenticator.java
@@ -1,6 +1,8 @@
 package AuthModule;
 
+import BBDD.ModelosBD.UsuarioSistemaEntity;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
 
 public class Authenticator {
@@ -11,9 +13,26 @@ public class Authenticator {
 
     }
 
-    public void createUser(){
-        UserRecord.CreateRequest req = new UserRecord.CreateRequest();
+    public void createUser(UsuarioSistemaEntity user){
+        UserRecord.CreateRequest req = new UserRecord.CreateRequest()
+                .setUid("dr23")
+                .setEmail("adrianluces111@gmail.com")
+                .setPassword("123456");
 
+        try {
+            UserRecord record = auth.createUser(req);
+        } catch (FirebaseAuthException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void deleteUser(UsuarioSistemaEntity user){
+        try {
+            auth.deleteUser("dr23");
+        } catch (FirebaseAuthException e) {
+            e.printStackTrace();
+        }
 
     }
 
